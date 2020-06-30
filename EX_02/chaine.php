ecb1016efdaac2763e52982dcaa0902a11dfa43d
Tom Schneider <tom.schneider@iseg.fr>
2020-06-30 16:37:20 +0200

    Create chaine.php

---
+++ EX_02/chaine.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title> Chaîne </title>
+</head>
+
+<body>
+<?php
+
+$str1 = "La maîtrise des fondamentaux du code et de la programmation dans les domaines du business,du marketing et de la communication est un avantage sérieux et utile"
+echo $str1;
+echo strlen($str1);
+echo str_word_count("La maîtrise des fondamentaux du code et de la programmation dans les domaines du business,du marketing et de la communication est un avantage sérieux et utile");
+
+?>
+</body>
+
+
+
+
+
+</html>
