4afcda1c63a22e0a589331e4a2fec02b103ef997
Tom Schneider <tom.schneider@iseg.fr>
2020-06-30 17:08:06 +0200

    Update chaine.php

--- EX_02/chaine.php
+++ EX_02/chaine.php
@@ -10,7 +10,12 @@ $str1 = "La maîtrise des fondamentaux du code et de la programmation dans les d
 echo $str1;
 echo strlen($str1);
 echo str_word_count("La maîtrise des fondamentaux du code et de la programmation dans les domaines du business,du marketing et de la communication est un avantage sérieux et utile");
+echo strtolower ($str1);
+echo strtoupper ($str1);
+echo str_shuffle ($str1);
 
+$str1 = substr($str1,0,-32);
+echo $str1;
 ?>
 </body>
 
